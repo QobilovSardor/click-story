44f374d80a446dfd45b2173692c4fa81c1c465d1
sardor <sardorqobilov555@gmail.com>
2023-06-06 15:41:00 +0500

    author-page-two finished

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 $(function () {
 
-	$('.hide-block').slideUp();
-	$('.hide-block-two').slideUp();
-	$('.hide-block-notification').slideUp();
+	$('.hide-block').slideUp(0);
+	$('.hide-block-two').slideUp(0);
+	$('.hide-block-notification').slideUp(0);
+	$('.hide-box').slideUp(0);
+	$('.bg-changer').slideUp(0);
+	$('.select-achievements').slideUp(0);
+
 	$('.read-more').on("click", function () {
 		$('.hide-block').slideToggle();
 
@@ -32,33 +36,26 @@ $(function () {
 			}
 		});
 	})
-
-	const selects = document.querySelectorAll('.select-menu');
-
-	selects.forEach(item => {
-		let select = item.querySelector('.select');
-		let options = item.querySelectorAll('.option');
-		let optionList = item.querySelector('.options-list');
-		select.onclick = function () {
-			this.classList.toggle('active');
-			this.classList.toggle('select_active')
-			optionList.classList.toggle('active');
-		}
-
-		options.forEach(option => {
-			option.onclick = function () {
-				options.forEach(a => {
-					a.classList.remove('selected');
-				})
-				this.classList.add('selected');
-				let parent = this.parentNode.parentNode;
-				let select = parent.querySelector('.select');
-				let options_list = parent.querySelector('.options-list');
-				select.classList.remove('active');
-				select.classList.remove('select_active');
-				options_list.classList.remove('active');
-				select.querySelector('span').innerHTML = this.innerHTML;
-			}
+	$('.change-btn').on("click", function () {
+		$('.bg-changer').slideToggle();
+		$('.select-achievements').slideUp();
+	});
+	$('.open-select__achievements').on("click", function () {
+		$('.select-achievements').slideToggle();
+	});
+	$('.select-achievements__close').on("click", function () {
+		$('.select-achievements').slideToggle();
+	});
+	$('.info-box').each(function (i, e) {
+		let openBoxBtn = e.querySelector('.open-box');
+		let subscribersBox = e.querySelectorAll('.hide-box');
+		let closeBtn = e.querySelectorAll('.subscribers-close');
+		$(openBoxBtn).on("click", function () {
+			$(subscribersBox).slideToggle();
+			console.log('salom');
+		})
+		$(closeBtn).on("click", function () {
+			$(subscribersBox).slideUp();
 		})
 	})
 
@@ -95,6 +92,33 @@ $(function () {
 		}, 1000);
 	});
 
+	const selects = document.querySelectorAll('.select-menu');
+
+	selects.forEach(item => {
+		let select = item.querySelector('.select');
+		let options = item.querySelectorAll('.option');
+		let optionList = item.querySelector('.options-list');
+		select.onclick = function () {
+			this.classList.toggle('active');
+			this.classList.toggle('select_active')
+			optionList.classList.toggle('active');
+		}
+
+		options.forEach(option => {
+			option.onclick = function () {
+				options.forEach(a => {
+					a.classList.remove('selected');
+				})
+				this.classList.add('selected');
+				let parent = this.parentNode.parentNode;
+				let select = parent.querySelector('.select');
+				let options_list = parent.querySelector('.options-list');
+				select.classList.remove('active');
+				select.classList.remove('select_active');
+				options_list.classList.remove('active');
+				select.querySelector('span').innerHTML = this.innerHTML;
+			}
+		})
+	})
+
 });
-
-
