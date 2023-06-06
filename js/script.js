cc3c2c8b558fa391132d321e8eb03b5f02e9edbd
sardor <sardorqobilov555@gmail.com>
2023-06-06 18:56:42 +0500

    settings.html finished

--- js/script.js
+++ js/script.js
@@ -92,6 +92,24 @@ $(function () {
 		}, 1000);
 	});
 
+	$('.cinema-big__box').each(function (i, e) {
+		let btn = e.querySelector('.cinema-box__hide-btn');
+		let topCol = e.querySelectorAll('.top-col');
+		console.log(btn);
+		$(btn).on("click", function() {
+			$(topCol).slideToggle();
+			$(this).toggleClass('rotate')
+		})
+	})
+
+	$('.change-text__btn').on('click', function () {
+		if ($(this).text() == "Отключить") {
+			$(this).text("Включить двухфакторную аутентификацию");
+		} else {
+			$(this).text("Отключить");
+		}
+	});
+
 	const selects = document.querySelectorAll('.select-menu');
 
 	selects.forEach(item => {
@@ -122,3 +140,63 @@ $(function () {
 	})
 
 });
+
+
+
+$(document).on("change", ".uploadProfileInput", function () {
+	var triggerInput = this;
+	var currentImg = $(this).closest(".pic-holder").find(".pic").attr("src");
+	var holder = $(this).closest(".pic-holder");
+	var wrapper = $(this).closest(".profile-pic-wrapper");
+	$(wrapper).find('[role="alert"]').remove();
+	triggerInput.blur();
+	var files = !!this.files ? this.files : [];
+	if (!files.length || !window.FileReader) {
+		return;
+	}
+	if (/^image/.test(files[0].type)) {
+		// only image file
+		var reader = new FileReader(); // instance of the FileReader
+		reader.readAsDataURL(files[0]); // read the local file
+
+		reader.onloadend = function () {
+			$(holder).addClass("uploadInProgress");
+			$(holder).find(".pic").attr("src", this.result);
+
+			// Dummy timeout; call API or AJAX below
+			setTimeout(() => {
+				$(holder).removeClass("uploadInProgress");
+				$(holder).find(".upload-loader").remove();
+				// If upload successful
+				if (Math.random() < 0.9) {
+
+					// Clear input after upload
+					$(triggerInput).val("");
+
+					setTimeout(() => {
+						$(wrapper).find('[role="alert"]').remove();
+					}, 3000);
+				} else {
+					$(holder).find(".pic").attr("src", currentImg);
+					$(wrapper).append(
+						'<div class="snackbar show" role="alert"><i class="fa fa-times-circle text-danger"></i> There is an error while uploading! Please try again later.</div>'
+					);
+
+					// Clear input after upload
+					$(triggerInput).val("");
+					setTimeout(() => {
+						$(wrapper).find('[role="alert"]').remove();
+					}, 3000);
+				}
+			}, 1500);
+		};
+	} else {
+		$(wrapper).append(
+			'<div class="alert alert-danger d-inline-block p-2 small" role="alert">Please choose the valid image.</div>'
+		);
+		setTimeout(() => {
+			$(wrapper).find('role="alert"').remove();
+		}, 3000);
+	}
+});
+
